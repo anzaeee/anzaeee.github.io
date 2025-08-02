77a7deda2bfd0ce34d27b8625520f66a06bbcca6
Shanzay Asad <shanzayyasad@gmail.com>
2025-08-02 22:21:33 +0500

    deploy fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,13 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/anzaeee-portfolio' : ''
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: '', // No subpath
+    },
+  },
 };
 
 export default config;
