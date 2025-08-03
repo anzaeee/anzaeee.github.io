fe6e7bf61fc307d8fc0f78a59b1e2f7d562cfc46
Shanzay Asad <shanzayyasad@gmail.com>
2025-08-03 11:58:23 +0500

    maybe these fixes are the one

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,10 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 const config = {
   preprocess: preprocess(),
-
   kit: {
-    adapter: adapter({
-      fallback: 'index.html',
-      // Remove strict here — it's not a valid option for adapter-static
-    }),
-    paths: {
-      base: '/project-name', 
-    },
-    prerender: {
-      entries: ['*'],
-    },
+    adapter: adapter(),
   },
 };
 
